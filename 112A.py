b49e3682b12b7b20fe8fef03494ffcee69b11278
Poornapragna Mathh <54894632+poornapragna877@users.noreply.github.com>
2020-10-01 09:23:37 +0530

    Update 112A.py
    Updated 112A.py

--- 112A.py
+++ 112A.py
@@ -1,8 +1,16 @@
-m = input().lower()
-n = input().lower()
-if m == n:
-    print("0")
-elif m > n :
-    print("1")
-else:
+m = input().lower()
+
+n = input().lower()
+
+
+if m == n:
+
+    print("0")
+
+elif m > n :
+
+    print("1")
+
+else:
+
     print ("-1")
